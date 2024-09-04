ec0f20474ef83277be9ed17ba58d3c51fc131bde
Adam MacKinnon <amackinnon@beyondtrust.com>
2024-09-04 16:24:15 -0300

    chore: update with initial customizations

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -45,4 +45,12 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+-- Keymaps for better default experience
+-- See `:help vim.keymap.set()`
+vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
+
+-- Remap for dealing with word wrap
+vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+
 -- vim: ts=2 sts=2 sw=2 et
